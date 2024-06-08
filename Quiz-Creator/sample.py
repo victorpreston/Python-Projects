f8f91ad9ccc74808274bfa96507f4cd3b315707d
Mano <112639423+Manavalan2517@users.noreply.github.com>
2024-06-08 15:54:24 +0530

    Projects added

---
+++ Quiz-Creator/sample.py
@@ -0,0 +1,7 @@
+from quiz import Quiz
+
+Quiz.process(question="Entomology is the science that studies", 
+             choices=["Behavior of human beings", "Insects", "The origin and history of technical and scientific terms", "The formation of rocks"], 
+             answer="b")
+
+Quiz.open_file()
